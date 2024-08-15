84f69f0c2a9cebfa66c4eed2c8372147f4e44918
Yuri Pires <yuripiresbr@gmail.com>
2024-08-15 18:31:28 -0300

    refactor: now home is a redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
-import Image from "next/image";
-import { getTasks } from "../../actions/actions";
-import { TaskStatus } from "./enums/TaskStatus";
+"use client";
+import { useEffect } from "react";
 
-type Task = {
-  id: number;
-  title: string;
-  description: string;
-  dueDate: string;
-  finishedAt: string;
-  status: TaskStatus;
-};
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const tasks = await getTasks();
-  return (
-    <div>
-      <h1>Lista de tasks</h1>
-      <ul>
-        {/* {tasks && tasks.length === 0 && <p>No tasks</p>} */}
-        {tasks.map((task: Task) => (
-          <li key={task.id}>
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
-            
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+export default function Home() {
+  useEffect(() => {
+    redirect("/tasks");
+  }, []);
+
+  return null;
 }
