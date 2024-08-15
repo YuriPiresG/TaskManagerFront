9f3469efbf5ed686648c66044e1c46a8ba6fb566
Yuri Pires <yuripiresbr@gmail.com>
2024-08-15 18:31:49 -0300

    chore: Add task details page and update task list page

---
+++ src/app/tasks/[id]/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { getTaskById } from "../../../../actions/actions";
+import { Task } from "../../types/Task";
+import { TaskStatus } from "@/app/enums/TaskStatus";
+import Link from "next/link";
+
+type Props = {
+  params: {
+    id: number;
+  };
+};
+
+const TaskDetails = async ({ params }: Props) => {
+  const task: Task = await getTaskById(params.id);
+  const dueDate = new Date(task.dueDate).toLocaleDateString("pt-br");
+  const finishedAt = task.finishedAt
+    ? new Date(task.finishedAt).toLocaleDateString("pt-br")
+    : null;
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-6 max-w-lg w-full">
+        <h1 className="text-2xl font-bold mb-4">{task.title}</h1>
+        <p className="text-gray-700 mb-2">
+          <span className="font-semibold">Id:</span> {task.id}
+        </p>
+        <p className="text-gray-700 mb-2">
+          <span className="font-semibold">Descrição:</span> {task.description}
+        </p>
+        <p className="text-gray-700 mb-2">
+          <span className="font-semibold">Prazo de validade:</span> {dueDate}
+        </p>
+        <p className="text-gray-700 mb-2">
+          <span className="font-semibold">Status:</span> {task.status}
+        </p>
+        {task.status === "COMPLETED" && (
+          <p className="text-gray-700 mb-2">
+            <span className="font-semibold">Finalizado:</span>
+            {task.status === TaskStatus.COMPLETED
+              ? finishedAt
+              : "ainda não finalizado"}
+          </p>
+        )}
+        <Link
+          href="/tasks"
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Voltar
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TaskDetails;
---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,21 @@
+import { getTasks } from "../../../actions/actions";
+import TaskCard from "../components/TaskCard";
+import ToggleTaskPage from "../components/ToggleTaskPage";
+import { Task } from "../types/Task";
+
+export default async function Home() {
+  const tasks = await getTasks();
+  return (
+    <div className="grid justify-center items-center">
+      <h1>Lista de tasks</h1>
+      <div className="grid grid-cols-5 gap-6">
+        {tasks.map((task: Task) => (
+          <TaskCard key={task.id} {...task} />
+        ))}
+        <div className="w-[15rem] h-[20rem] font-light grid rounded-xl justify-center items-center border bg-green-300 border-gray-400 shadow-2xl">
+          <ToggleTaskPage />
+        </div>
+      </div>
+    </div>
+  );
+}
