be7d81eebd139743a1b2488f10e5a3ec003a34ef
Yuri Pires <yuripiresbr@gmail.com>
2024-08-15 18:40:41 -0300

    style: updated UI

--- src/app/components/TaskCard.tsx
+++ src/app/components/TaskCard.tsx
@@ -1,11 +1,26 @@
 import Link from "next/link";
+import { FaCheckCircle, FaHourglassHalf, FaTimesCircle } from "react-icons/fa";
 import { TaskStatus } from "../enums/TaskStatus";
 import { Task } from "../types/Task";
 
-const statusMap: { [key in TaskStatus]: string } = {
-  [TaskStatus.COMPLETED]: "Concluída",
-  [TaskStatus.IN_PROGRESS]: "Em Progresso",
-  [TaskStatus.PENDING]: "Pendente",
+const statusMap: {
+  [key in TaskStatus]: { label: string; icon: JSX.Element; color: string };
+} = {
+  [TaskStatus.COMPLETED]: {
+    label: "Concluída",
+    icon: <FaCheckCircle />,
+    color: "text-green-500",
+  },
+  [TaskStatus.IN_PROGRESS]: {
+    label: "Em Progresso",
+    icon: <FaHourglassHalf />,
+    color: "text-blue-500",
+  },
+  [TaskStatus.PENDING]: {
+    label: "Pendente",
+    icon: <FaTimesCircle />,
+    color: "text-red-500",
+  },
 };
 
 const TaskCard = (task: Task) => {
@@ -25,21 +40,28 @@ const TaskCard = (task: Task) => {
 
   return (
     <div
-      className={`w-[15rem] h-[20rem] font-light grid rounded-xl justify-center items-center border ${back} border-gray-400 shadow-2xl p-4`}
+      className={`w-[15rem] h-[20rem] font-light grid rounded-xl justify-center items-center border ${back} border-gray-300 shadow-lg p-4 transition-transform transform hover:scale-105`}
     >
-      <h2 className="border-b border-black text-center font-bold text-xl break-words break-all mb-2">
+      <h2 className="border-b border-gray-300 text-center font-semibold text-lg break-words mb-4">
         {task.title.length > 20 ? task.title.slice(0, 20) + "..." : task.title}
       </h2>
-      <p className="mb-2">Status: {statusMap[task.status]}</p>
-      <p className="mb-2">Prazo: {dueDate.toLocaleDateString("pt-br")}</p>
-      {task.status === TaskStatus.COMPLETED ? (
-        <p className="mb-2">Finalizada: {finishedAt!.toDateString()}</p>
-      ) : (
-        <div />
-      )}
+      <div className="flex items-center mb-4">
+        <span className={`mr-2 ${statusMap[task.status].color}`}>
+          {statusMap[task.status].icon}
+        </span>
+        <p className="text-sm">{statusMap[task.status].label}</p>
+      </div>
+      <p className="text-sm mb-4">
+        Prazo: {dueDate.toLocaleDateString("pt-br")}
+      </p>
+
+      <p className="text-sm mb-4">
+        Finalizada: {finishedAt ? finishedAt.toLocaleDateString("pt-br") : "Não"}
+      </p>
+
       <Link
         href={`/tasks/${task.id}`}
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        className="bg-blue-500 text-white text-center py-2 px-4 rounded hover:bg-blue-600 transition-colors"
       >
         Detalhes
       </Link>
--- src/app/components/ToggleTaskPage.tsx
+++ src/app/components/ToggleTaskPage.tsx
@@ -1,30 +1,46 @@
 "use client";
 import React, { useState } from "react";
 import CreateUpdateForm from "./CreateUpdateForm";
+import { FaPlus } from "react-icons/fa"; // Importing the Plus icon from react-icons
 
 const ToggleTaskPage: React.FC = () => {
-  const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState<boolean>(false);
 
-  const handleButtonClick = () => {
-    setShowForm(!showForm);
+  const handleButtonClick = (): void => {
+    setShowForm((prevShowForm) => !prevShowForm);
   };
 
-  const handleCloseForm = () => {
+  const handleCloseForm = (): void => {
     setShowForm(false);
   };
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl mb-4">Tarefas</h1>
-      <button
-        onClick={handleButtonClick}
-        className="bg-blue-500 text-white p-2 rounded mb-4"
-      >
-        {showForm ? "Fechar Formulário" : "Abrir Formulário"}
-      </button>
-      {showForm && <CreateUpdateForm onClose={handleCloseForm} />}
+      <h1 className="text-3xl font-bold mb-6 text-center">Tarefas</h1>
+      <div className="flex justify-center mb-6">
+        <AddButton onClick={handleButtonClick} />
+      </div>
+      {showForm && (
+        <div className="flex justify-center">
+          <CreateUpdateForm onClose={handleCloseForm} />
+        </div>
+      )}
     </div>
   );
 };
+
+interface AddButtonProps {
+  onClick: () => void;
+}
+
+const AddButton: React.FC<AddButtonProps> = ({ onClick }) => (
+  <button
+    onClick={onClick}
+    className="bg-blue-500 text-white p-3 rounded-full flex items-center transition-transform transform hover:scale-105"
+  >
+    <FaPlus className="h-5 w-5 mr-2" />
+    Adicionar
+  </button>
+);
 
 export default ToggleTaskPage;
